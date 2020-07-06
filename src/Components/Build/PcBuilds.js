35ee03ec98059e56ba5971ce46d2ff35cfd667f8
Patrick <patrickmarin@gmail.com>
2020-07-06 17:31:39 -0500

    Searchable Input PC Builds

--- src/Components/Build/PcBuilds.js
+++ src/Components/Build/PcBuilds.js
@@ -13,25 +13,27 @@ import keyboard from "../../icons/keyboard.png"
 import mouse from "../../icons/mouse.png"
 import headset from "../../icons/headset.png"
 import pccase from "../../icons/pccase.png"
+import Select from 'react-select'
 
   function PcBuilds(){
   
   const{user, parts, checkLoginStatus, loggedInStatus} = useContext(Context)
   const history = useHistory()
   
-  const Mobo = parts.map(part => part.part_type === "Mobo" ? (<option value = {part.id}>{part.description}</option>): "")
-  const CPU = parts.map(part => part.part_type === "CPU" ? (<option value = {part.id}>{part.description}</option>): "")
-  const CPUCooler = parts.map(part => part.part_type === "CPUCooler" ? (<option value = {part.id}>{part.description}</option>): "")
-  const GPU = parts.map(part => part.part_type === "GPU" ? (<option value = {part.id}>{part.description}</option>): "")
-  const RAM = parts.map(part => part.part_type === "RAM" ? (<option value = {part.id}>{part.description}</option>): "")
-  const HD = parts.map(part => part.part_type === "HD" ? (<option value = {part.id}>{part.description}</option>): "")
-  const Case = parts.map(part => part.part_type === "Case" ? (<option value = {part.id}>{part.description}</option>): "")
-  const PWS = parts.map(part => part.part_type === "PWS" ? (<option value = {part.id}>{part.description}</option>): "")
-  const Monitor = parts.map(part => part.part_type === "Monitor" ? (<option value = {part.id}>{part.description}</option>): "")
-  const Keyboard = parts.map(part => part.part_type === "Keyboard" ? (<option value = {part.id}>{part.description}</option>): "")
-  const Mouse = parts.map(part => part.part_type === "Mouse" ? (<option value = {part.id}>{part.description}</option>): "")
-  const Headset = parts.map(part => part.part_type === "Headset" ? (<option value = {part.id}>{part.description}</option>): "")
+  const Mobo = (parts.filter(part => part.part_type === "Mobo")).map(a=>{return {value: a.id,label: a.description}})
+  const CPU = (parts.filter(part => part.part_type === "CPU")).map(a=>{return {value: a.id,label: a.description}})
+  const CPUCooler = (parts.filter(part => part.part_type === "CPUCooler")).map(a=>{return {value: a.id,label: a.description}})
+  const GPU = (parts.filter(part => part.part_type === "GPU")).map(a=>{return {value: a.id,label: a.description}})
+  const RAM = (parts.filter(part => part.part_type === "RAM")).map(a=>{return {value: a.id,label: a.description}})
+  const HD = (parts.filter(part => part.part_type === "HD")).map(a=>{return {value: a.id,label: a.description}})
+  const Case = (parts.filter(part => part.part_type === "Case")).map(a=>{return {value: a.id,label: a.description}})
+  const PWS = (parts.filter(part => part.part_type === "PWS")).map(a=>{return {value: a.id,label: a.description}})
+  const Monitor = (parts.filter(part => part.part_type === "Monitor")).map(a=>{return {value: a.id,label: a.description}})
+  const Keyboard = (parts.filter(part => part.part_type === "Keyboard")).map(a=>{return {value: a.id,label: a.description}})
+  const Mouse = (parts.filter(part => part.part_type === "Mouse")).map(a=>{return {value: a.id,label: a.description}})
+  const Headset = (parts.filter(part => part.part_type === "Headset")).map(a=>{return {value: a.id,label: a.description}})
  
+  console.log(Mobo)
 
   function createBuild(e) {
     const form = new FormData(document.getElementById("newPcbuild"));
@@ -89,7 +91,7 @@ import pccase from "../../icons/pccase.png"
         <th>Icon</th>
         <th className = "component">Component</th>
         <th className = "component-description">Description</th>
-        <th className = "component-price">Price</th>
+        <th className = "hola">Price</th>
       </tr>
     </thead>
     <tbody>
@@ -97,15 +99,15 @@ import pccase from "../../icons/pccase.png"
         <td><img src={mobo} className="build-icon"/></td>
         <td className = "component">Motherboard</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[Mobo][id]"
-                className="dropdown"
-                id = "mobo">
-              {Mobo}
-              </select>
+              <Select 
+              name="part_id[Mobo][id]"
+              required
+              options= {Mobo}
+              className= "component-description"
+              placeholder="Select or type..."
+              /> 
         </td>
-        <td className = "component-price">
+        <td className = "part-price">
                 <input
                   type="text"
                   name="part_id[Mobo][price]"
@@ -119,12 +121,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={cpu} className="build-icon" /></td>
         <td className = "component">CPU</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[CPU][id]"
-                className="dropdown">
-              {CPU}
-              </select>
+              <Select 
+              name="part_id[CPU][id]"
+              required
+              options= {CPU}
+              placeholder="Select or type..."
+              /> 
         </td>
         <td className = "component-price">
                 <input
@@ -139,12 +141,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={cpucooler} className="build-icon"/></td>
         <td className = "component">CPU Cooler</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[CPUCooler][id]"
-                className="dropdown">
-              {CPUCooler}
-              </select>
+            <Select 
+              name="part_id[CPUCooler][id]"
+              required
+              options= {CPUCooler}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -159,12 +161,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={gpu} className="build-icon"/></td>
         <td className = "component">Video Card</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[GPU][id]"
-                className="dropdown">
-              {GPU}
-              </select>
+        <Select 
+              name="part_id[GPU][id]"
+              required
+              options= {GPU}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -179,12 +181,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={ram} className="build-icon"/></td>
         <td className = "component">RAM</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[RAM][id]"
-                className="dropdown">
-              {RAM}
-              </select>
+              <Select 
+              name="part_id[RAM][id]"
+              required
+              options= {RAM}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -199,12 +201,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={hdd} className="build-icon" /></td>
         <td className = "component">Hard Drive 1</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[HD][id]"
-                className="dropdown">
-              {HD}
-              </select>
+             <Select 
+              name="part_id[HD][id]"
+              required
+              options= {HD}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -219,12 +221,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={hdd} className="build-icon" /></td>
         <td className = "component">Hard Drive 2</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[HD][id]"
-                className="dropdown">
-              {HD}
-              </select>
+              <Select 
+              name="part_id[HD][id]"
+              required
+              options= {HD}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -239,12 +241,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={pccase} className="build-icon"/></td>
         <td className = "component">Case</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[Case][id]"
-                className="dropdown">
-              {Case}
-              </select>
+              <Select 
+              name="part_id[Case][id]"
+              required
+              options= {Case}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -259,12 +261,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={pws} className="build-icon"/></td>
         <td className = "component">Power Supply</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[PWS][id]"
-                className="dropdown">
-              {PWS}
-              </select>
+             <Select 
+              name="part_id[PWS][id]"
+              required
+              options= {PWS}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -279,12 +281,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={monitor} className="build-icon"/></td>
         <td className = "component">Monitor</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[Monitor][id]"
-                className="dropdown">
-              {Monitor}
-              </select>
+             <Select 
+              name="part_id[Monitor][id]"
+              required
+              options= {Monitor}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -299,12 +301,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={keyboard} className="build-icon"/></td>
         <td className = "component">Keyboard</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[Keyboard][id]"
-                className="dropdown">
-              {Keyboard}
-              </select>
+              <Select 
+              name="part_id[Keyboard][id]"
+              required
+              options= {Keyboard}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -319,12 +321,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={mouse} className="build-icon" /></td>
         <td className = "component">Mouse</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[Mouse][id]"
-                className="dropdown">
-              {Mouse}
-              </select>
+              <Select 
+              name="part_id[Mouse][id]"
+              required
+              options= {Mouse}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
@@ -339,12 +341,12 @@ import pccase from "../../icons/pccase.png"
         <td><img src={headset} className="build-icon" /></td>
         <td className = "component">Headset</td> 
         <td className = "component-description">
-              <select
-                type="select"
-                name="part_id[Headset][id]"
-                className="dropdown">
-              {Headset}
-              </select>
+              <Select 
+              name="part_id[Headset][id]"
+              required
+              options= {Headset}
+              placeholder="Select or type..."
+              />
         </td>
         <td className = "component-price">
                 <input
