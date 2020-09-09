700a8097cb943545347a2d030f97a9ba399b1133
Patrick <patrickmarin@gmail.com>
2020-09-09 12:13:07 -0500

    Sitemap Generator

---
+++ src/sitemap-routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Route} from "react-router-dom";
+
+
+export default (
+        <Route>
+          <Route exact path = "/"></Route>
+          <Route path= "/allbuilds"></Route>
+          <Route  exact path = "/pcbuilds"></Route>
+          <Route  path = "/pcbuilds/edit/:buildId"> </Route>
+          <Route path = "/builds/:buildId"></Route>
+          <Route path= "/favorites"> </Route>
+          <Route path = "/signIn"> </Route>
+          <Route path = "/register"> </Route>
+          <Route path = "/forgotPassword"> </Route>
+          <Route path = "/resetPassword"> </Route>
+          <Route exact path = "/profile/:userId"> </Route>
+          <Route path = "/profile/edit/:userId"> </Route>
+          <Route exact path = "/forum/"></Route>
+          <Route path = "/forum/:forumId"></Route>
+          <Route exact path = "/discussions/"></Route>
+          <Route  path = "/discussions/:discussionId"></Route>
+          <Route  path = "/newdiscussion/"></Route>
+          <Route  path = "/editdiscussion/:discussionId"></Route>
+          <Route  path = "/:discussionId/editpost/:postId"></Route>
+          <Route  path = "/editbuildpost/:buildpostId"></Route>
+        </Route>
+    );
