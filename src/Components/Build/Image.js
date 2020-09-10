bc47ba7e4ade9b00dcf74019d28905cf18460422
Patrick <patrickmarin@gmail.com>
2020-09-10 16:04:39 -0500

    Reduced font size Main Card

--- src/Components/Build/Image.js
+++ src/Components/Build/Image.js
@@ -97,9 +97,9 @@ function Image({img}) {
                 </div>
                 <hr className="main-card-hr"></hr>
                 <div className= "build-name-card">{img.build_name}</div>
-                <div style={{fontSize: "13px"}}>{CPU}</div>
-                <div style={{fontSize: "13px"}}>{Mobo}</div>
-                <div style={{fontSize: "13px"}}>{GPU}</div>
+                <div style={{fontSize: "12px"}}>{CPU}</div>
+                <div style={{fontSize: "12px"}}>{Mobo}</div>
+                <div style={{fontSize: "12px"}}>{GPU}</div>
                 <div>...</div>
                 
               </div>
